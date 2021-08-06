fdbc0ba711a374fafa9b2f68db9282187db20530
Miles Danswan <miles.danswan@gmail.com>
2021-08-06 22:06:14 +1000

    ADD: Window Configuration and Non-decorative frame movement

--- JIT-Utilities/MainWindow.xaml.cs
+++ JIT-Utilities/MainWindow.xaml.cs
@@ -20,9 +20,33 @@ namespace JIT_Utilities
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Point? posOld = null;
+        private Point? posNew = null;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            posOld = Mouse.GetPosition(this);
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (posOld != null)
+            {
+                posNew = Mouse.GetPosition(this);
+                Left += posNew.Value.X - posOld.Value.X;
+                Top += posNew.Value.Y - posOld.Value.Y;
+            }
+        }
+
+        private void Window_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            posOld = null;
+            posNew = null;
         }
     }
 }
